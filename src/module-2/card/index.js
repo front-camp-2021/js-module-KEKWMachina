9a4c6e40ffa4482571f0b41555dda39c5a645cbb
Maksym Rudenko <m4ksym.rudenko@gmail.com>
2021-09-28 13:56:43 +0300

    Module-2 uses getTemplate()

--- src/module-2/card/index.js
+++ src/module-2/card/index.js
@@ -17,79 +17,50 @@ export default class Card {
     this.price = price;
     this.category = category;
     this.brand = brand;
+
     this.render();
   }
 
+  getTemplate () {
+    return `<section class="card">
+    <div class="card-img">
+        <img src="${this.images[0]}" alt="Merchendise image">
+    </div>
+    
+    <div class="rating-and-price">
+        <div class="rating">
+            <span>${this.rating}</span>
+            <div class="icon-rating"></div>
+        </div>
+        <span class="price">$${this.price}</span>
+    </div>
+    <ul class="card-ul">
+        <li class="item-name">${this.title}</li>
+        <li class="item-description">Redesigned from scratch and completely revised.</li>
+    </ul>
+    <div class="wishlist-cart-btn">
+        <button class="wishlist"> <div class="wishlist-img"></div> WISHLIST</button>
+        <button class="cart"> <div class="cart-img"></div> ADD TO CART</button>
+    </div>
+    </section>`
+  }
+
   render () {
-    const cardWrapper = document.createElement('section');
-    cardWrapper.className = 'card';
+    const wrapper = document.createElement('div');
 
-    const cardImageWrapper = document.createElement('div');
-    cardImageWrapper.className = 'card-img';
+    wrapper.innerHTML = this.getTemplate();
 
-    const cardImg = new Image();
-    cardImg.src = this.images[0];
+    this.element = wrapper.firstElementChild;
+  }
 
-    const ratingAndPrice = document.createElement('div');
-    ratingAndPrice.className = 'rating-and-price';
+  remove () {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
 
-    const ratingWrapper = document.createElement('div');
-    ratingWrapper.className = 'rating';
-
-    const ratingIcon = document.createElement('div');
-    ratingIcon.className = 'icon-rating';
-
-    const ratingNumber = document.createElement('span');
-    ratingNumber.className = 'rating-number';
-
-    const price = document.createElement('span');
-    price.className = 'price';
-
-    const merchandiseDescriptionList = document.createElement('ul');
-    merchandiseDescriptionList.className = 'card-ul';
-
-    const merchandiseName = document.createElement('li');
-    merchandiseName.className = 'item-name';
-
-    const merchandiseDecs = document.createElement('li');
-    merchandiseDecs.className = 'item-description';
-
-    const wishlistAndCartWrapper = document.createElement('div');
-    wishlistAndCartWrapper.className = 'wishlist-cart-btn';
-
-    const wishlistButton = document.createElement('button');
-    wishlistButton.className = 'wishlist';
-
-    const addToCartButton = document.createElement('button');
-    addToCartButton.className = 'cart';
-
-    cardImageWrapper.append(cardImg);
-    cardWrapper.append(cardImageWrapper);
-    cardWrapper.append(ratingAndPrice);
-    cardWrapper.append(merchandiseDescriptionList);
-    cardWrapper.append(wishlistAndCartWrapper);
-
-    ratingAndPrice.append(ratingWrapper);
-    ratingAndPrice.append(price);
-
-    ratingWrapper.append(ratingNumber);
-    ratingWrapper.append(ratingIcon);
-    
-    merchandiseDescriptionList.append(merchandiseName);
-    merchandiseDescriptionList.append(merchandiseDecs);
-
-    wishlistAndCartWrapper.append(wishlistButton);
-    wishlistAndCartWrapper.append(addToCartButton);
-
-    
-    price.textContent = `${this.price}₴`;
-    ratingNumber.textContent = this.rating;
-    merchandiseName.textContent = this.title;
-    merchandiseDecs.textContent = 'Completely Revised';
-    wishlistButton.textContent = 'WISHLIST';
-    addToCartButton.textContent = 'ADD TO CART';
-    this.element = cardWrapper;
+  destroy () {
+    this.remove();
+    this.element = null;
   }
 }
-
-
