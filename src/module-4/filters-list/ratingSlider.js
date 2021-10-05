dd55a1d64e89225a4b4c83dfef9234a1b31b8bff
Maksym Rudenko <m4ksym.rudenko@gmail.com>
2021-10-05 14:09:18 +0300

    made filters-list dispatch events

---
+++ src/module-4/filters-list/ratingSlider.js
@@ -0,0 +1,87 @@
+/* eslint-disable radix */
+export default class ratingSlider {
+    element;
+
+    constructor ({
+        min = 0,
+        max = 0,
+        filterName = 'No filters found'
+    } = {}) {
+        this.min = min,
+        this.max = max,
+        this.filterName = filterName,
+        this.render();
+    }
+
+    get template() {
+        return `<div class="wrapper">
+        <div class="values">
+            <span>${this.filterName}</span>
+            <span class="range-1">
+            ${this.min}
+            </span>
+            <span> &dash; </span>
+            <span class="range-2">
+            ${this.max}
+            </span>
+        </div>
+        <div class="container">
+          <div class="slider--track"></div>
+          <input type="range" min="${this.min}" max="${this.max}" value="${this.min}" class="slider--1">
+          <input type="range" min="${this.min}" max="${this.max}" value="${this.max}" class="slider--2">
+        </div>
+      </div>
+`
+    }
+
+    
+    render() {
+        const wrapper = document.createElement('div');
+
+        wrapper.innerHTML = this.template;
+        
+        this.element = wrapper.firstElementChild;
+    }
+
+    rangeSelector() {
+        const sliderOne = document.querySelector(".slider--1");
+        const sliderTwo = document.querySelector(".slider--2");
+        const displayValOne = document.querySelector(".range-1");
+        const displayValTwo = document.querySelector(".range-2");
+        const minGap = 0;
+        const sliderTrack = document.querySelector(".slider--track");
+        const sliderMaxValue = document.querySelector(".slider--1").max;
+
+        function slideOne() {
+            if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
+              sliderOne.value = parseInt(sliderTwo.value) - minGap;
+            }
+            displayValOne.textContent = sliderOne.value;
+            fillColor();
+          }
+      
+          function slideTwo() {
+            if (parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
+              sliderTwo.value = parseInt(sliderOne.value) + minGap;
+            }
+            displayValTwo.textContent = sliderTwo.value;
+            fillColor();
+          }
+      
+          function fillColor() {
+            const percent1 = (sliderOne.value / sliderMaxValue) * sliderMaxValue;
+            const percent2 = (sliderTwo.value / sliderMaxValue) * sliderMaxValue;
+            sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${100}% , #7e72f2 ${percent1}% , #7e72f2 ${percent2}%, #dadae5 ${100}%)`;
+          }
+      
+          fillColor();
+      
+          sliderOne.addEventListener('change', () => {
+            slideOne();
+          });
+      
+          sliderTwo.addEventListener('change', () => {
+            slideTwo();
+          });
+    }
+}
