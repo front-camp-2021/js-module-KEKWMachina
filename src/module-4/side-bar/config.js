b48354a9d4bcdb8c83471bb85ca7effeafc6620b
Maksym Rudenko <m4ksym.rudenko@gmail.com>
2021-10-05 22:20:40 +0300

    module-4 side-Bar done, tests passed
    	deleted:    src/module-4/side-bar/index.js

--- src/module-4/side-bar/config.js
+++ src/module-4/side-bar/config.js
@@ -1,37 +1,38 @@
 export const categoryFilterConfig = [
-  {
-    value: 'category=cell_phones',
-    title: 'Cell Phones',
-  },
-  {
-    value: 'category=computer_tablets',
-    title: 'Computers & Tablets',
-  },
-  {
-    value: 'category=cell_phones_accessories',
-    title: 'Cell Phone Accessories',
-  },
-  {
-    value: 'category=appliances',
-    title: 'Appliances',
-  },
-  {
-    value: 'category=audio',
-    title: 'Audio',
-  }
-];
-
-export const brandFilterConfig = [
-  {
-    value: 'brand=insigni',
-    title: 'Insigni',
-  },
-  {
-    value: 'brand=samsung',
-    title: 'Samsung',
-  },
-  {
-    value: 'brand=apple',
-    title: 'Apple',
-  }
-];
+    {
+      value: 'category=cell_phones',
+      title: 'Cell Phones',
+    },
+    {
+      value: 'category=computer_tablets',
+      title: 'Computers & Tablets',
+    },
+    {
+      value: 'category=cell_phones_accessories',
+      title: 'Cell Phone Accessories',
+    },
+    {
+      value: 'category=appliances',
+      title: 'Appliances',
+    },
+    {
+      value: 'category=audio',
+      title: 'Audio',
+    }
+  ];
+  
+  export const brandFilterConfig = [
+    {
+      value: 'brand=insigni',
+      title: 'Insigni',
+    },
+    {
+      value: 'brand=samsung',
+      title: 'Samsung',
+    },
+    {
+      value: 'brand=apple',
+      title: 'Apple',
+    }
+  ];
+  
